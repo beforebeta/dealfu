6cc34536e027e88733fe53603651ceef194bed64
makkalot <makkalot@gmail.com>
2014-02-21 17:38:35 +0200

    adding more filtering to the api

--- dealfuapi/dealfu/dealfu/esutils.py
+++ dealfuapi/dealfu/dealfu/esutils.py
@@ -69,7 +69,7 @@ class EsDealsQuery(EsHandleMixin):
 
     @property
     def query(self):
-        #print "QUERYYYYYYYYY ",self._query
+        print "QUERYYYYYYYYY ",self._query
         result = self.handle.search(index=self.index,
                                     doc_type=self.doc_type,
                                     body=self._query)
@@ -110,3 +110,80 @@ class EsDealsQuery(EsHandleMixin):
         self._query["query"]["filtered"]["query"] = q
         return self
 
+
+
+    def filter_online(self, online):
+        """
+        Filters the online true false
+        """
+        boolq = self._get_filter_bool()
+        if boolq.get("must"):
+            boolq["must"].append({"term":{"online":online}})
+        else:
+            boolq["must"] = [{"term":{"online":online}}]
+
+        return self
+
+
+    def filter_category_slugs(self, category_slugs):
+        """
+        Filters a list of category slugs
+        """
+        shoulds = [{"term":{"category_slug":c}} for c in category_slugs]
+
+        boolq = self._get_filter_bool()
+        if boolq.get("should"):
+            boolq["should"].extend(shoulds)
+        else:
+            boolq["should"] = shoulds
+
+        return self
+
+
+    def filter_page(self, page=0, per_page=10):
+        """
+        Filters per page
+        """
+        self._query["from"]= page
+        self._query["size"] = per_page
+
+        return self
+
+    def _get_filter_bool(self):
+        """
+        Gets the bool part of filter query
+        """
+        filtered = self._query["query"]["filtered"]
+        if not filtered.get("filter"):
+           self._query["query"]["filtered"].update(self._get_default_filter())
+
+        filtered = self._query["query"]["filtered"]
+
+        boolq = filtered["filter"]["bool"]
+        return boolq
+
+
+    def order_by(self, asc_list, desc_list):
+        """
+        Orders the query by supplied parameters
+        """
+        asc_sorts = [{o:"asc"} for o in asc_list]
+        desc_sorts = [{o:"desc"} for o in desc_list]
+
+        sort_dict = {"sort" : asc_sorts + desc_sorts}
+        self._query.update(sort_dict)
+
+        return self
+
+
+
+
+    def _get_default_filter(self):
+        """
+        The default filter query
+        """
+        return {
+            "filter":{
+                "bool":{}
+            }
+        }
--- dealfuapi/dealfu/dealfu/views.py
+++ dealfuapi/dealfu/dealfu/views.py
@@ -1,13 +1,31 @@
 from dealfu.esutils import EsDeals, EsDealsQuery
-from django.conf import settings
 from django.http import Http404
-
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 from dealfu.serializers import DealSerializer
+
+
+def _get_order_lists(order_str):
+    """
+    By default the order field is a little bit complex
+    so we have to split by comma and then filter those
+    that are ending with _desc
+    """
+    asc_list = []
+    desc_list = []
+
+    ordered_list = [o.strip() for o in order_str.split(",") if o.strip()]
+    if not ordered_list:
+        return (), ()
+
+    asc_list = filter(lambda o:not o.endswith("_desc"), ordered_list)
+    desc_list = filter(lambda o:o.endswith("_desc"), ordered_list)
+    desc_list = [o.replace("_desc","") for o in desc_list]
+
+    return asc_list, desc_list
 
 
 
@@ -18,9 +36,40 @@ class DealsListView(APIView):
         Gets a list of the deals from db
         """
         es = EsDealsQuery()
+        params = request.QUERY_PARAMS
 
-        if request.QUERY_PARAMS.get("query"):
-            es = es.filter_query(request.QUERY_PARAMS.get("query"))
+        if params.get("query"):
+            es = es.filter_query(params.get("query"))
+
+        if params.get("online"):
+            es = es.filter_online(params.get("online"))
+
+        if params.get("category_slugs"):
+            cs = params.get("category_slugs")
+            category_slugs = [c.strip() for c in cs.split(",")]
+            es = es.filter_category_slugs(category_slugs)
+
+
+        if params.get("order"):
+            asc, desc = _get_order_lists(params.get("order"))
+            es = es.order_by(asc, desc)
+
+
+        #put those in settings
+        page = 0
+        per_page = 10
+
+        if params.get("page"):
+            page = int(params.get("page"))
+
+        if params.get("per_page"):
+            per_page = int(params.get("per_page"))
+
+
+        #the pagination will be here !
+        es = es.filter_page(page=page,
+                            per_page=per_page)
+
 
         docs = es.query
         serializer = DealSerializer(instance=docs, many=True)
