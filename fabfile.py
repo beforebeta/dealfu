18580fbef052d841c5984c6e10f14785404050be
makkalot <makkalot@gmail.com>
2014-02-25 14:12:17 +0200

    clean fabric file, more to come ...

--- fabfile.py
+++ fabfile.py
@@ -1,84 +1,39 @@
 import os
 import sys
 import json
-import time
-
+import re
+import yaml
 
 from fabric.api import *
 
-SETTINGS_DIR = "settings"
-PROJECT_DIR = "scrapers/dealfu_groupon/"
-PROJECT_NAME = "dealfu"
+SETTINGS_DIR = "settings/deploy/inventory/group_vars/"
 
-def devel():
-    _get_conf("devel")
+def vagrant():
+    env.host_type = "remote"
+    env.port = "2222"
+    env.hosts = ["192.168.0.113"]
+    env.user = "vagrant"
+    _get_conf("vagrant")
+
 
 def staging():
+    env.host_type = "remote"
+    env.port = "22"
+    env.hosts = ["dealfu.dfvops.com"]
+    env.user = "root"
     _get_conf("staging")
 
 
 def production():
-    _get_conf("production")
-
+    raise NotImplemented
 
 
 def full_deploy(branch_deploy="master"):
     """
     Prepares the remote machine for deployment
     """
-    with hide('running', 'stdout', 'stderr'):
-        with settings(warn_only=True):
-            res = _execute("virtualenv -h").failed
-    
-    if res:
-        #then you should install it fistly
-        _execute("sudo apt-get -y install python-virtualenv")
-
-    #at that stage we have virtualenv now go further and create a venv
-    clean_deploy()
-
-    _execute("sudo mkdir -p %s"%env.conf.DEPLOY_DIR)
-    _execute("sudo chown -R %s %s"%(env.user, env.conf.DEPLOY_DIR))
-    
-    
-    #we will need git before go further
-    _execute("sudo apt-get -y install git")
-    _execute("sudo apt-get -y install python-dev")
-
-    #needed for lxml
-    _execute("sudo apt-get -y install libxml2-dev libxslt-dev")
-
-    #some base packages neede
-    _execute("sudo apt-get -y install software-properties-common")
-    _execute("sudo apt-get -y install python-software-properties")
-
-
-    #install ES for db access
-    _execute("sudo add-apt-repository ppa:webupd8team/java")
-    _execute("sudo apt-get update")
-    #_execute("sudo apt-get -y install oracle-java7-installer") run manually
-    _execute("java -version")
-
-    _install_es()
-
-
-    with _cdir(env.conf.DEPLOY_DIR):
-        #now create the virtualenv
-        _execute("virtualenv --no-site-packages venv")
-        #now clone the code into that directory
-        _execute("git clone %s sjimporter"%(env.conf.GIT_ADDR))
-
-
-def install_es():
-    deb_add = "deb http://packages.elasticsearch.org/elasticsearch/0.90/debian stable main"
-    _execute("echo '%s' | sudo tee -a /etc/apt/sources.list"%deb_add)
-    _execute("sudo apt-get update")
-    _execute("sudo apt-get -y --force-yes install elasticsearch")
-    _execute("sudo service elasticsearch start")
-    time.sleep(2)
-    #test if it works
-    _execute("sudo apt-get install curl")
-    _execute("curl -X GET 'http://localhost:9200'")
+    #run the ansible here
+    pass
 
 
 
@@ -86,12 +41,8 @@ def update_deploy(branch_deploy="develop"):
     """
     Updates the system if have any changes they are reflected
     """
-    with _cdir(os.path.join(env.conf.DEPLOY_DIR, "sjimporter")):
-        _execute("git pull origin %s"%branch_deploy)
-
-        #the next step is to run requirements.txt
-        _virtualenv("pip install -r requirements.txt")
-
+    #call here only the appropriate ansibal tasks
+    pass
 
 
 def clean_deploy():
@@ -102,9 +53,9 @@ def clean_deploy():
     _execute("if [ -d %s ]; then sudo rm -rf %s; fi"%(env.deploy_dir, env.deploy_dir))
     
 
-
 def status_system():
     pass
+
 
 def start_system():
     """
@@ -121,107 +72,40 @@ def stop_system():
 
 
 
-import elasticsearch
-from elasticsearch.client import IndicesClient
-
-def _get_es():
-    """
-    Internal util for es
-    """
-    es_host = env.conf.ES_SERVER
-    es_port = env.conf.ES_PORT
-
-    d = {
-        "host":es_host,
-        "port":es_port
-    }
-
-    return elasticsearch.Elasticsearch(hosts=[d])
-
-
-def es_index_create(index):
-    """
-    Creates an index on ES server
-    """
-
-    es = IndicesClient(_get_es())
-    print es.create(index=index)
-
-
-def es_index_delete(index):
-    """
-    Deletes the index
-    """
-    es = IndicesClient(_get_es())
-    print es.delete(index=index)
-
-
-def es_list_mapping(index, mtype):
-    """
-    Lists mappings from specififed type and indexes
-    """
-    pass
-
-
-
-def es_create_mapping(index):
-    """
-    Creates a mapping for specified type from resources file
-    """
-    resource_file = os.path.join(env.conf.DEPLOY_DIR,
-                                 PROJECT_DIR,
-                                 "resources/mappings.json")
-
-    mapping_str = open(resource_file, "r").read()
-    mappings = json.loads(mapping_str)
-
-    #print json.dumps(mp, indent=4, sort_keys=True)
-    es = IndicesClient(_get_es())
-
-    for k,v in mappings.iteritems():
-        print es.put_mapping(index, k, {k:mappings[k]})
-
-
-
-
-
-def es_drop_mapping(index, mtype):
-    """
-    Drops mapping for specified mtype
-    """
-    pass
-
-
-def insert_fixture(fixture):
-    """
-    Inserts some data into ES for testing
-    """
-    pass
-
-
-
-
-
 
 #For internal usage do not call those
+def _resolve_var(s, conf):
+    """
+    Resolves a variable against the given dict
+    @param s: in the form of ${x.y}
+    @param conf: in the form of dict
+    """
+    pass
+
+
+
 def _get_conf(name):
     """
     Loads the configuration from settings directory
     """
     #import first the settings
     cur_dir = os.getcwd()
-    sys.path.append(cur_dir)
+    settings_path = os.path.join(cur_dir,
+                                 SETTINGS_DIR)
 
-    __import__("%s"%SETTINGS_DIR, globals(), locals(), [], -1)
-    conf = __import__("%s.%s"%(SETTINGS_DIR, name), globals(), locals(), [], -1)
-    conf = getattr(conf, name)
-    env.host_type = conf.HOST_TYPE
-    env.port = conf.PORT
-    env.hosts = conf.HOSTS
-    env.user = conf.SSH_USER
-    env.deploy_dir = conf.DEPLOY_DIR
+    settings_file = os.path.join(settings_path,
+                                 ".".join([name, "yml"]))
+
+    if os.path.exists(settings_file):
+        raise Exception("invalid settings supplied")
+
+
+
+    conf = yaml.load(open(settings_file, "r"))
+    env.deploy_dir = conf["project"]["root"]
     env.conf = conf
     env.env_type = name
+
 
 
 def _execute(*args, **kwargs):
