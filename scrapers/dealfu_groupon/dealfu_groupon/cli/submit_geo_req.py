0e9b9278a9267680d50c3bfd6989e000e14684c8
makkalot <makkalot@gmail.com>
2014-04-04 20:00:13 +0300

    filter out the parts that don't have price and merchant.name

--- scrapers/dealfu_groupon/dealfu_groupon/cli/submit_geo_req.py
+++ scrapers/dealfu_groupon/dealfu_groupon/cli/submit_geo_req.py
@@ -1,6 +1,7 @@
 import cli.app
 from dealfu_groupon.background.geocode import submit_geo_request
-from dealfu_groupon.utils import get_es, es_get_batch, get_default_logger, needs_geo_fetch, needs_address_geo_fetch
+from dealfu_groupon.utils import get_es, es_get_batch, get_default_logger, needs_geo_fetch, needs_address_geo_fetch, \
+    get_in, from_obj_settings
 from scrapy.utils.project import get_project_settings
 
 logger = get_default_logger("dealfu_groupon.cli.deal_checker")
@@ -15,7 +16,8 @@ def submit_geo_request_addresses(app):
     """
     settings = get_project_settings()
 
-    query = {   "from": 0,
+    query = {
+        "from": 0,
         "size":100,
         "query":
         {
@@ -31,6 +33,12 @@ def submit_geo_request_addresses(app):
                         }},
                         {"exists": {
                             "field": "merchant.addresses.address"
+                        }},
+                        {"exists": {
+                            "field": "merchant.name"
+                        }},
+                        {"exists": {
+                            "field": "price"
                         }}
                  ]
              }
@@ -51,10 +59,13 @@ def submit_geo_request_addresses(app):
                 #logger.warn("The item {} have blanks in address but is not sent for geofetch".format(item_id))
                 continue
 
-            for addr in item.get("addresses", []):
+            for addr in get_in(item, "merchant","addresses"):
+                #logger.info("Check address : {}".format(addr))
                 if needs_address_geo_fetch(addr):
                     logger.debug("Submit geo {} for addr : {}".format(item_id, addr))
-                    submit_geo_request.delay(settings, item_id)
+                    #logger.debug("Settings : {}".format(from_obj_settings(settings.settings_module)))
+                    submit_geo_request.delay(from_obj_settings(settings.settings_module),
+                                             item_id)
 
         #now that batch is processed get a new one
         from_page += per_page
